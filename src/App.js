ab580f91b6511c4d62b38b9f7d2803dcba59544d
woonbeom <baek12201@gmail.com>
2020-03-23 00:15:16 +0900

    todos state 만들고 TodoList에 전달하여 그리기
    - App에서 todos state 만들고 props로 TodoList에 전달.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
 
 const App = () => {
+  // 여러 종류의 값을 전달해야 하는 경우에는 객체로 한 번에 넘기는게 성능 최적화 면에서도 좋다.
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: '리액트의 기초 알아보기',
+      checked: true,
+    },
+    {
+      id: 2,
+      text: '컴포넌트 스타일링해 보기',
+      checked: true,
+    },
+    {
+      id: 3,
+      text: '일정 관리 앱 만들업 보기',
+      checked: false,
+    },
+  ]);
   return (
     <TodoTemplate>
       <TodoInsert />
-      <TodoList />
+      <TodoList todos={todos} />
     </TodoTemplate>
   );
 };
