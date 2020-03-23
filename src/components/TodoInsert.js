f178247496f6590e6d067e70305b6b648e500cb4
woonbeom <baek12201@gmail.com>
2020-03-23 23:17:37 +0900

    onSubmit 이벤트 설정하여 todo 추가하는 동작 구현

--- src/components/TodoInsert.js
+++ src/components/TodoInsert.js
@@ -2,15 +2,24 @@ import React, { useState, useCallback } from 'react';
 import { MdAdd } from 'react-icons/md';
 import './TodoInsert.scss';
 
-const TodoInsert = () => {
+const TodoInsert = ({ onInsert }) => {
   const [value, setValue] = useState('');
 
   const onChange = useCallback(e => {
     setValue(e.target.value);
   }, []);
 
+  const onSubmit = useCallback(
+    e => {
+      onInsert(value);
+      setValue(''); // value 값 초기화.
+      e.preventDefault(); // submit 이벤트는 새로고침 동작이 동반됨. 이를 방지.
+    },
+    [onInsert, value],
+  );
+
   return (
-    <form className="TodoInsert">
+    <form className="TodoInsert" onSubmit={onSubmit}>
       <input
         placeholder="할 일을 입력하세요."
         value={value}
